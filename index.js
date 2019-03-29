d14e985aa8ec603a61ee81bb1bb23b150e21e2eb
Francis Mendez <fmendez884@gmail.com>
2019-03-29 15:45:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 function dwarfRollCall(dwarves) {
-  roll = []
-  i = 0
+  let roll = []
+  let i = 0
     while (i < dwarves.length){
-      roll.push(`${i+1}. ${dwarves[i]}`)
+      roll.push(`${i+1}. ${dwarves[i]} `)
         i++
     }
-    return roll.join(' ') 
+    return roll.join('') 
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (let i = 0; i < planeteerCalls.length; i++){
+   planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!";
+  }
+  return planeteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++){
+    if (words[i].length > 4) {
+      return true
+    }
+    else { 
+    return false
+    }
+  }
 }
 
-function findTheCheese (foods) {
-}
+function findTheCheese(foods) {
+  for (let i = 0; i < foods.length; i++){
+    if (foods[i] === "gouda" || foods[i] === "cheddar" || foods[i] === "camembert"){
+      return foods[i]
+    }
+  }
+  return "no cheese!"
+} 
