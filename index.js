fa9ec9dadb086c553c259891285ff6ff14d0b787
Francis Mendez <fmendez884@gmail.com>
2019-03-29 12:58:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function dwarfRollCall(dwarves) {
+  roll = []
+  i = 0
+    while (i < dwarves.length){
+      roll.push(`${i+1}. ${dwarves[i]}`)
+        i++
+    }
+    return roll.join(' ') 
 }
 
 function summonCaptainPlanet(planeteerCalls){
